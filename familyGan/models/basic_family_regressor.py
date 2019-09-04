1d36cb3166a2e5a8585c11ee1665deab372abbc4
idanbrus <skandal0>
2019-09-04 20:05:22 +0300

    added the first models

---
+++ familyGan/models/basic_family_regressor.py
@@ -0,0 +1,7 @@
+from sklearn.base import BaseEstimator
+
+
+class BasicFamilyReg(BaseEstimator):
+    def __init__(self, seed=42):
+        self.seed = 42
+
---
+++ familyGan/models/simple_avarage.py
@@ -0,0 +1,17 @@
+from familyGan.models.basic_family_regressor import BasicFamilyReg
+import numpy as np
+import config
+
+
+class SimpleRegressor(BasicFamilyReg):
+    def __init__(self, direction=config.age_kid_direction, coef=1):
+        self.direction = direction
+        self.coef = coef
+
+    def fit(self, X_fathers, X_mothers, y_child):
+        pass
+
+    def predict(self, X_fathers, X_mothers):
+        y_pred_old = np.mean([X_fathers, X_mothers], axis=-1)
+        y_pred_young = y_pred_old + self.coef * self.direction
+        return y_pred_young
