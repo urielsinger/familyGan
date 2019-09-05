73629f3e43d8cd68e4ff5df00e1c807188f386f0
noam <noambox@gmail.com>
2019-09-05 23:50:00 +0300

    torch correction

--- familyGan/models/simple_avarage.py
+++ familyGan/models/simple_avarage.py
@@ -14,6 +14,12 @@ class SimpleAverageModel(BasicFamilyReg):
         pass
 
     def predict(self, X_fathers, X_mothers):
-        y_pred_old = np.mean([X_fathers, X_mothers], axis=0)
+        gender_direction = np.random.choice([2, -2], size=X_fathers.shape[0])
+        if self.bgd_norm:
+            latent_index = self.background_heuristic(X_fathers,X_mothers)
+            y_pred_mean = np.mean([X_fathers, X_mothers], axis=0)
+            y_pred_old = X_fathers * (~latent_index) + y_pred_mean * latent_index
+        else:
+            y_pred_old = np.mean([X_fathers, X_mothers], axis=0)
         y_pred_young = y_pred_old + self.coef * self.direction
-        return self.add_random_gender(y_pred_young)
+        return self.add_random_gender(y_pred_young, coefs=gender_direction)
