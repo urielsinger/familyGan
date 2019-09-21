9a87bf7707e70b7d65c737cd4b4941e98297e362
Noam Cohen <noambox@gmail.com>
2019-09-21 21:51:53 +0300

    - init stylegan plus

--- familyGan/run.py
+++ familyGan/run.py
@@ -8,12 +8,31 @@ from familyGan.stylegan_encoder.training.misc import load_pkl, save_pkl
 
 ITER = 450
 LR = 2.  # not used ATM, uses on in perceptual_model.py
-EXPERIMENT_NAME = f"init_iter_{ITER}_lr_{LR}"
+EXPERIMENT_NAME = f"adam_iter_{ITER}_lr_{LR}"
 IM_PATH = "../data/toy_face.jpg"
 IM2_PATH = "../data/toy_face2.jpg"
 RESULTS_PATH = "../results"
 DLATENTS_CACHE = pjoin(RESULTS_PATH, "cache/dlatents")
 VGG_EMBED_CACHE = pjoin(RESULTS_PATH, "cache/vgg_embeddings")
+
+def run_single_image():
+    im = Image.open(IM_PATH)
+
+    #  image2latent -
+    im_aligned = align_image(im)
+    init_dlatent = load_pkl(pjoin(DLATENTS_CACHE,f"toy_image_dlatent.pkl"))
+
+    start = time.time()
+    print(f"started im2lat")
+    _, aligned_latent = image2latent(im_aligned, iterations=ITER, learning_rate=LR, init_dlatent = None)
+    end = time.time()
+    print(f"took {end - start} sec")
+
+    # latent2image
+    save_pkl(aligned_latent, pjoin(DLATENTS_CACHE,f"toy_image_dlatent.pkl"))
+    im_hat = latent2image(aligned_latent)
+    im_hat.save(pjoin(RESULTS_PATH,f'{round(end - start,3)}_sec_'+ EXPERIMENT_NAME +'.png'))
+
 
 def run_2_images():
     im = Image.open(IM_PATH)
@@ -41,5 +60,6 @@ def run_2_images():
 
 
 if __name__ == '__main__':
-    for i in range(1):
-        run_2_images()
+    # for i in range(1):
+    #     run_2_images()
+    run_single_image()
